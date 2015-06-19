f3940fca7d51f618193c7d212c5dabefcf7ebfc4
JeffCui55 <jeffcui55@gmail.com>
2015-06-19 13:16:52 -0400

    Button to see Screen

--- app/src/main/java/com/choppingboard/v1/DashBoard.java
+++ app/src/main/java/com/choppingboard/v1/DashBoard.java
@@ -1,7 +1,9 @@
 package com.choppingboard.v1;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by Gray on 6/19/2015.
@@ -12,6 +14,18 @@ public class DashBoard extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+    }
+
+    public void seeScreen(View view){
+        Intent intent = new Intent(DashBoard.this, SeeScreen.class);
+        DashBoard.this.startActivity(intent);
+        DashBoard.this.finish();
+    }
+    public void buildScreen(View view){
+
+    }
+    public void invoiceScreen(View view){
 
     }
 }
