7dc53cb7b0bdde3d2a00084ff941fcdae46cc657
JeffCui55 <jeffcui55@gmail.com>
2015-06-19 14:55:58 -0400

    Swiping

--- app/src/main/java/com/choppingboard/v1/SeeScreen.java
+++ app/src/main/java/com/choppingboard/v1/SeeScreen.java
@@ -5,9 +5,15 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+import com.wdullaer.swipeactionadapter.SwipeActionAdapter;
+import com.wdullaer.swipeactionadapter.SwipeDirections;
 
 
-public class SeeScreen extends ListActivity {
+public class SeeScreen extends ListActivity implements SwipeActionAdapter.SwipeActionListener {
+
+    SwipeActionAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +23,24 @@ public class SeeScreen extends ListActivity {
         String[] cars = {"Order 1" ,"Order 2", "Order 3","Order 4","Order 5","Order 6","Order 7","Order 8"};
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(getListView().getContext(), android.R.layout.simple_list_item_1, cars);
         getListView().setAdapter(adapter);
+
+        mAdapter = new SwipeActionAdapter(adapter);
+        mAdapter.setSwipeActionListener(this)
+                .setDimBackgrounds(true)
+                .setListView(getListView());
+        setListAdapter(mAdapter);
+
+
+        mAdapter
+                .addBackground(SwipeDirections.DIRECTION_FAR_LEFT,R.layout.leftswipe)
+                .addBackground(SwipeDirections.DIRECTION_NORMAL_LEFT,R.layout.leftswipe)
+                .addBackground(SwipeDirections.DIRECTION_FAR_RIGHT,R.layout.rightswipe)
+                .addBackground(SwipeDirections.DIRECTION_NORMAL_RIGHT, R.layout.rightswipe);
+
+
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -39,5 +62,44 @@ public class SeeScreen extends ListActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean hasActions(int i) {
+        return true;
+    }
+
+    @Override
+    public boolean shouldDismiss(int i, int i1) {
+        return false;
+    }
+
+    @Override
+    public void onSwipe(int[] ints, int[] ints1) {
+        for(int i=0;i<ints.length;i++) {
+            int direction = ints1[i];
+            int position = ints[i];
+            String dir = "";
+
+            switch (direction) {
+                case SwipeDirections.DIRECTION_FAR_LEFT:
+                    dir = "Far left";
+                    break;
+                case SwipeDirections.DIRECTION_NORMAL_LEFT:
+                    dir = "Left";
+                    break;
+                case SwipeDirections.DIRECTION_FAR_RIGHT:
+                    dir = "Far right";
+                    break;
+                case SwipeDirections.DIRECTION_NORMAL_RIGHT:
+                    dir = "Right";
+                    break;
+            }
+            Toast.makeText(
+                    SeeScreen.this,dir + " swipe Action triggered on " + mAdapter.getItem(position),
+                    Toast.LENGTH_SHORT
+            ).show();
+            mAdapter.notifyDataSetChanged();
+        }
     }
 }
