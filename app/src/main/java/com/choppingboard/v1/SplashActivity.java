3ecbbdab33bd6431170c087b472707a993c3db47
BuildTools <unconfigured@null.spigotmc.org>
2015-06-19 11:21:50 -0400

    first commit

---
+++ app/src/main/java/com/choppingboard/v1/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.choppingboard.v1;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+/**
+ * Created by Gray on 6/19/2015.
+ */
+public class SplashActivity extends Activity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashActivity.this, DashBoard.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
