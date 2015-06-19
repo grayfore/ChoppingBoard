34530ea9e5e7e98efb2741582d92c5d8c6c885f6
JeffCui55 <jeffcui55@gmail.com>
2015-06-19 12:23:58 -0400

    Adding things to manifest and making dashboard an activity

--- app/src/main/java/com/choppingboard/v1/DashBoard.java
+++ app/src/main/java/com/choppingboard/v1/DashBoard.java
@@ -1,7 +1,17 @@
 package com.choppingboard.v1;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by Gray on 6/19/2015.
  */
-public class DashBoard {
+public class DashBoard extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+    }
 }
